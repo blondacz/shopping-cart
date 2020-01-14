1c0d35f279d34ff3265bc5709dd19e7725532d06
blondacz <tomas.klubal@gmail.com>
2020-01-14 12:19:22 +0000

    Step 0 - scala, sbt, scalatest setup

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "shopping-cart"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.1.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0" % "test"
---
+++ src/test/scala/net/softwaria/cart/DummyTest.scala
@@ -0,0 +1,10 @@
+package net.softwaria.cart
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class DummyTest extends AnyFreeSpec with Matchers {
+  "some test" in {
+    1 shouldBe 1
+  }
+}
