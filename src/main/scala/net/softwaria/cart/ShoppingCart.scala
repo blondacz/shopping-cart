9d3f8532dd2de1373027626957c7c6c54a255f30
blondacz <tomas.klubal@gmail.com>
2020-01-14 16:29:04 +0000

    Step 3 - More efficient calculation
    Offers calculated during one iteration

--- src/main/scala/net/softwaria/cart/ShoppingCart.scala
+++ src/main/scala/net/softwaria/cart/ShoppingCart.scala
@@ -9,6 +9,15 @@ object ShoppingCart {
     Apple -> BigDecimal("0.60"),
     Orange -> BigDecimal("0.25"))
 
-  val totalCostWithoutOffers : Cart => Option[BigDecimal] = items => items.foldLeft(Option(Zero))((tc, i) => tc.flatMap(c => prices.get(i).map(_ + c)))
-  val totalCost : Cart => Option[BigDecimal] = cart => totalCostWithoutOffers(cart).map(_ - Offer.allOffers.map(Offer.applyOffer(_,cart)).sum)
+  val totalCostWithoutOffers: Cart => Option[BigDecimal] = items => items.foldLeft(Option(Zero))((tc, i) => tc.flatMap(c => prices.get(i).map(_ + c)))
+
+  val totalCost: Cart => Option[BigDecimal] = cart => {
+    val (totalCost, discounts) = cart.foldLeft((Option(Zero), Offer.allOffers)) {
+      case ((tc, offers), item) =>
+        val newCost = tc.flatMap(c => prices.get(item).map(_ + c))
+        val newOffers = offers.map(_.withItem(item))
+        (newCost, newOffers)
+    }
+    totalCost.map(_ - discounts.map(_.totalDiscount).sum)
+  }
 }
